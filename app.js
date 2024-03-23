8ec4e28d1f953f534cdb104f55b866a4d426628e
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-03-23 23:16:40 +0900

    setting
    - express-generator 로 시작
    - pug viewer 등 불필요한 패키지,코드 정리
    - prettier 설정
    - .gitignore 설정
    - accounts router 설정

---
+++ app.js
@@ -0,0 +1,29 @@
+var createError = require("http-errors");
+var express = require("express");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var accountsRouter = require("./routes/accounts");
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/accounts", accountsRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+	next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+	// render the error page
+	res.status(err.status || 500);
+	res.send("server error: " + err.message);
+});
+
+module.exports = app;
