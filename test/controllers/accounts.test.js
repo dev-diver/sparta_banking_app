c4fed9a2a201a602f1f8035d7c24e3632aa7759e
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-03-24 23:34:30 +0900

    입금 출금 테스트

--- test/controllers/accounts.test.js
+++ test/controllers/accounts.test.js
@@ -23,29 +23,29 @@ async function createNewAccount(name) {
 
 async function checkAccount(id) {
 	const response = await request(app)
-		.get("/accounts/" + id) // 실제 경로로 변경하세요.
+		.get(`/accounts/${id}`) // 실제 경로로 변경하세요.
 		.send()
 		.expect(200);
 	return response.body;
 }
 
-describe("계정 생성 test", () => {
+describe("계정 생성과 조회 set", () => {
 	beforeEach(() => {
 		app = createApp();
 	});
 
-	it("POST / 이름이 같은 새 계정을 만들어야 함", async () => {
+	it("새 계정 만들기", async () => {
 		const response = await createNewAccount("스파르타");
 		verifyNewAccount(response, "스파르타", "1");
 	});
 
-	it("GET /:accountId id가 같은 계정을 transaction과 함께 반환해야 함.", async () => {
+	it("새 계정 만들고 조회", async () => {
 		await createNewAccount("스파르타");
 		const response = await checkAccount("1");
 		verifyNewAccount(response, "스파르타", "1");
 	});
 
-	it("여러 계정을 만들고 조회해도 id가 같은 계정을 transaction과 함께 반환해야 함.", async () => {
+	it("여러 계정 만들고 순서 바꿔 조회", async () => {
 		await createNewAccount("스파르타");
 		await createNewAccount("스파게티");
 		await createNewAccount("마가리타");
@@ -55,5 +55,93 @@ describe("계정 생성 test", () => {
 		verifyNewAccount(response1, "스파르타", "1");
 		verifyNewAccount(response2, "스파게티", "2");
 		verifyNewAccount(response3, "마가리타", "3");
+	});
+});
+
+async function deposit(id, amount) {
+	const response = await request(app)
+		.post(`/accounts/${id}/deposit`) // 실제 경로로 변경하세요.
+		.send({ amount: amount })
+		.expect(200);
+	return response.body;
+}
+
+async function withdraw(id, amount, status = 200) {
+	const response = await request(app)
+		.post(`/accounts/${id}/withdraw`) // 실제 경로로 변경하세요.
+		.send({ amount: amount })
+		.expect(status);
+	return response.body;
+}
+
+async function transfer(id, recipientId, amount) {
+	const response = await request(app)
+		.post(`/accounts/${id}/transfer`) // 실제 경로로 변경하세요.
+		.send({ amount: amount })
+		.expect(200);
+	return response.body;
+}
+
+function checkTransaction(transaction, Ttype, amountChangeType, amount, balance) {
+	expect(transaction.Ttype).toEqual(Ttype);
+	expect(transaction.amountChangeType).toEqual(amountChangeType);
+	expect(transaction.amount).toEqual(amount);
+	expect(transaction.balance).toEqual(balance);
+}
+
+describe("입금과 출금 set", () => {
+	beforeEach(async () => {
+		app = createApp();
+		await createNewAccount("스파르타");
+	});
+
+	it("입금", async () => {
+		let response = await deposit("1", 50);
+		let data = response.data;
+		checkTransaction(data, "DEPOSIT", 0, 50, 50);
+		console.log(data);
+
+		let accountResponse = await checkAccount("1");
+		data = accountResponse.data;
+		expect(data.account.balance).toBe(50);
+		checkTransaction(data.transactions[data.transactions.length - 1], "DEPOSIT", 0, 50, 50);
+	});
+
+	it("입금 후 출금", async () => {
+		await deposit("1", 150);
+		let response = await withdraw("1", 100);
+		let data = response.data;
+		checkTransaction(data, "WITHDRAWAL", 1, 100, 50);
+		console.log(data);
+
+		let accountResponse = await checkAccount("1");
+		data = accountResponse.data;
+		expect(data.account.balance).toBe(50);
+		checkTransaction(data.transactions[data.transactions.length - 1], "WITHDRAWAL", 1, 100, 50);
+	});
+
+	it("계좌보다 많은 돈 출금", async () => {
+		await deposit("1", 150);
+		let response = await withdraw("1", 400, 400);
+		console.log(response);
+		expect(response.success).toBe(false);
+		expect(response.error).toBe("계좌보다 많은 돈을 인출할 수 없습니다.");
+
+		let accountResponse = await checkAccount("1");
+		let data = accountResponse.data;
+		expect(data.account.balance).toBe(150);
+		checkTransaction(data.transactions[data.transactions.length - 1], "DEPOSIT", 0, 150, 150);
+	});
+
+	it("입금보다 많은 돈 출금", async () => {
+		let response = await withdraw("1", 100, 400);
+		console.log(response);
+		expect(response.success).toBe(false);
+		expect(response.error).toBe("계좌보다 많은 돈을 인출할 수 없습니다.");
+
+		let accountResponse = await checkAccount("1");
+		let data = accountResponse.data;
+		expect(data.account.balance).toBe(0);
+		expect(data.transactions.length).toBe(0);
 	});
 });
